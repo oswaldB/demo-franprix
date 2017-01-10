d3bcf201b4587cc92fa0b32375140e67fad76e7d
Oswald BERNARD <oswald.bernard@metidia.com>
2017-01-10 08:49:58 +0100

    final version

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1484033645,
+	"version": 1484034408,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -13,6 +13,8 @@
 		"images/sprite7-sheet0.png",
 		"images/sprite8-sheet0.png",
 		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
